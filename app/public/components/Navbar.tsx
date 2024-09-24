d6dbf6d56933d2252c39de9a3dd1238b67c7fddb
hassan402-paymentrequired <codewithhassan.404.not.found@gmail.com>
2024-09-24 16:57:48 +0100

    new commit

--- app/public/components/Navbar.tsx
+++ app/public/components/Navbar.tsx
@@ -1,20 +1,73 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
-import Link from 'next/link'
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { TiUserOutline } from "react-icons/ti";
+import { FaThemeco } from "react-icons/fa";
+import { GrProjects } from "react-icons/gr";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const Navbar = () => {
   return (
-    <div  className="w-full h-20 bg-transparent fixed flex items-center justify-center p-3">
-      <div className="flex px-2 h-full bg-gray-700 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 rounded">
-        <ul className='flex md:space-x-5 space-x-2 w-full items-center justify-center'>
-          <li className='md:text-sm text-xs font-bold px-2 text-white'><Link href='#'>Home</Link></li>
-          <li className='md:text-sm text-xs font-bold px-2 text-white'><Link href='#'>Contact</Link></li>
-          <li className='md:text-sm text-xs font-bold px-2 text-white'><Link href='#'>Project</Link></li>
-          <Button className='bg-lime-400 text-black shadow-2xl shadow-lime-400 hover:bg-lime-700 group'>Say Hello <span className='group-hover:animate-ping'>👋</span></Button>
+    <div className="top-[40%] rotate-90 bg-transparent absolute -right-5 flex items-center justify-center p-3">
+      <div className="flex p-2 h-full  bg-gray-500 bg-clip-padding backdrop-filter  backdrop-blur bg-opacity-50 saturate-100 backdrop-contrast-100 rounded-[2rem]">
+        <ul className="flex md:space-x-3 space-x-2 w-full items-center justify-center">
+          <li className="md:text-sm text-xs font-bold px-2 text-white">
+            <Link href="#">
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    {" "}
+                    <TiUserOutline size={20} />
+                  </TooltipTrigger>
+                  <TooltipContent className="top-2 -left-40 -rotate-90 absolute">
+                    <p>home</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </Link>
+          </li>
+          <li className="md:text-sm text-xs font-bold px-2 text-white">
+            <Link href="#">
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    {" "}
+                         <FaThemeco size={20}/> 
+                  </TooltipTrigger>
+                  <TooltipContent className="top-2 -left-40 -rotate-90 absolute">
+                    <p>service</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </Link>
+          </li>
+          <li className="md:text-sm text-xs font-bold px-2 text-white">
+            <Link href="#">
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    {" "}
+              <GrProjects size={20}/>
+                  </TooltipTrigger>
+                  <TooltipContent className="top-2 -left-40 -rotate-90 absolute">
+                    <p>Project</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </Link>
+          </li>
+          <Button className="bg-lime-400 w-10 h-10 rounded-full text-black shadow-2xl shadow-lime-400 hover:bg-lime-700 group">
+            <span className="group-hover:animate-ping">👋</span>
+          </Button>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- components/Stack.tsx
+++ components/Stack.tsx
@@ -1,30 +1,40 @@
-import { ExperinceCard } from "./ExperinceCard"
-import SkillTag from "./SkillTag"
-import { CardDescription, CardTitle } from "./ui/card"
+import Image from "next/image";
+import { ExperinceCard } from "./ExperinceCard";
+import SkillTag from "./SkillTag";
+import { CardDescription, CardTitle } from "./ui/card";
+import js from "../app/public/images/js.png";
+import vs from "../app/public/images/vs.jpg";
+import stm from "../app/public/images/stm.png";
+import php from "../app/public/images/php.png";
 
 const Stack = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-3 p-10">
-            <ExperinceCard/>
-        <div className="grid grid-cols-1 grid-rows-2 gap-y-4 ">
-            <div className="bg-neutral-950 border border-gray-800 rounded-md p-5 ">
-            <CardTitle>Skills and Expertise</CardTitle>
-                <SkillTag/>
-            </div>
-            <div className="bg-neutral-950 rounded-md p-5 border border-gray-800">
-            <CardTitle>Essential Stacks</CardTitle>
-            <CardDescription className="mt-3">A comprehensive collect of usefull tools to support my workflow</CardDescription>
-            <div className="flex items-center space-x-2">
-                
-            </div>
-            </div>
+      <ExperinceCard />
+      <div className="grid grid-cols-1 grid-rows-2 gap-y-4 ">
+        <div className="bg-neutral-950 border border-gray-800 rounded-md p-5 ">
+          <CardTitle>Skills and Expertise</CardTitle>
+          <SkillTag />
         </div>
+        <div className="bg-neutral-950 rounded-md p-5 border border-gray-800">
+          <CardTitle>Essential Stacks</CardTitle>
+          <CardDescription className="mt-3">
+            A comprehensive collect of usefull tools to support my workflow
+          </CardDescription>
+          <div className="flex items-center space-x-5 mt-3">
+            <Image src={js} width={40} className="rounded-md"  alt="vs" />
+            <Image src={vs} width={40} className="rounded-md"  alt="vs" />
+            <Image src={php} width={40} className="rounded-md"  alt="vs" />
+            <Image src={stm} width={40} className="rounded-md"  alt="stm" />
+          </div>
+        </div>
+      </div>
 
-        <div className="bg-gray-800 border border-gray-800 bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-15 rounded-md p-5">
+      <div className="bg-gray-800 border border-gray-800 bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-15 rounded-md p-5">
         <CardTitle>Connects</CardTitle>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Stack
+export default Stack;
